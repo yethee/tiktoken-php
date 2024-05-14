aee76850ad0cc247e1b78c0e1eda2cc96860e97b
yethee <yethee@users.noreply.github.com>
2024-05-14 14:07:06 +0300

    Fix tests
    Mock fs to fix issue when temporary directory is read only

--- tests/Vocab/Loader/DefaultVocabLoaderTest.php
+++ tests/Vocab/Loader/DefaultVocabLoaderTest.php
@@ -4,81 +4,54 @@ declare(strict_types=1);
 
 namespace Yethee\Tiktoken\Tests\Vocab\Loader;
 
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
 use PHPUnit\Framework\TestCase;
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
 use Yethee\Tiktoken\Vocab\Loader\DefaultVocabLoader;
 
-use function copy;
 use function dirname;
-use function file_put_contents;
+use function file_get_contents;
 use function hash;
-use function rmdir;
-use function sys_get_temp_dir;
-use function unlink;
 
 final class DefaultVocabLoaderTest extends TestCase
 {
-    private string $cacheDir;
+    private vfsStreamDirectory $cacheDir;
 
     public function testLoadFromCache(): void
     {
-        $loader = new DefaultVocabLoader($this->cacheDir);
-
+        $loader = new DefaultVocabLoader($this->cacheDir->url());
         $vocabUrl = 'http://localhost/cl100k_base.tiktoken';
-        $cacheFile = $this->cacheDir . '/' . hash('sha1', $vocabUrl);
+        $expectedHash = '223921b76ee99bde995b7ff738513eef100fb51d18c93597a113bcffe865b2a7';
 
-        copy(dirname(__DIR__, 2) . '/Fixtures/cl100k_base.tiktoken', $cacheFile);
-        self::assertFileEquals(dirname(__DIR__, 2) . '/Fixtures/cl100k_base.tiktoken', $cacheFile);
+        vfsStream::newFile(hash('sha1', $vocabUrl))
+            ->withContent(file_get_contents(dirname(__DIR__, 2) . '/Fixtures/cl100k_base.tiktoken'))
+            ->at($this->cacheDir);
 
-        $vocab = $loader->load($vocabUrl, '223921b76ee99bde995b7ff738513eef100fb51d18c93597a113bcffe865b2a7');
+        $vocab = $loader->load($vocabUrl, $expectedHash);
 
         self::assertSame(100256, $vocab->count());
     }
 
     public function testInvalidateCacheWhenChecksumMismatch(): void
     {
-        $loader = new DefaultVocabLoader($this->cacheDir);
-
+        $loader = new DefaultVocabLoader($this->cacheDir->url());
         $vocabUrl = dirname(__DIR__, 2) . '/Fixtures/p50k_base.tiktoken';
-        $cacheFile = $this->cacheDir . '/' . hash('sha1', $vocabUrl);
+        $expectedHash = '94b5ca7dff4d00767bc256fdd1b27e5b17361d7b8a5f968547f9f23eb70d2069';
 
-        file_put_contents($cacheFile, 'outdated content');
-        self::assertFileExists($cacheFile);
+        $cacheFile = vfsStream::newFile(hash('sha1', $vocabUrl))
+            ->withContent('outdated content')
+            ->at($this->cacheDir);
 
-        $vocab = $loader->load($vocabUrl, '94b5ca7dff4d00767bc256fdd1b27e5b17361d7b8a5f968547f9f23eb70d2069');
+        $vocab = $loader->load($vocabUrl, $expectedHash);
 
         self::assertSame(50280, $vocab->count());
 
-        self::assertFileExists($cacheFile);
-        self::assertFileEquals($vocabUrl, $cacheFile);
+        self::assertFileExists($cacheFile->url());
+        self::assertFileEquals($vocabUrl, $cacheFile->url());
     }
 
     protected function setUp(): void
     {
-        $this->cacheDir = sys_get_temp_dir() . '/tiktoken-test';
-
-        self::removeDir($this->cacheDir);
-    }
-
-    protected function tearDown(): void
-    {
-        self::removeDir($this->cacheDir);
-    }
-
-    private static function removeDir(string $path): void
-    {
-        $iterator = new RecursiveIteratorIterator(
-            new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS),
-            RecursiveIteratorIterator::CHILD_FIRST,
-        );
-
-        foreach ($iterator as $entry) {
-            if ($entry->isFile()) {
-                unlink($entry->getPathname());
-            } else {
-                rmdir($entry->getPathname());
-            }
-        }
+        $this->cacheDir = vfsStream::setup('cache');
     }
 }
