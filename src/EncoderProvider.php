ab363873b85bfbaf9a1135a7cdd73b4a797f9d05
yethee <yethee@users.noreply.github.com>
2024-05-14 12:16:49 +0300

    Add support GPT-4o

--- src/EncoderProvider.php
+++ src/EncoderProvider.php
@@ -36,14 +36,21 @@ final class EncoderProvider implements ResetInterface
             'vocab' => 'https://openaipublic.blob.core.windows.net/encodings/cl100k_base.tiktoken',
             'pat' => '/(?i:\'s|\'t|\'re|\'ve|\'m|\'ll|\'d)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]+|\s+(?!\S)|\s+/u',
         ],
+        'o200k_base' => [
+            'vocab' => 'https://openaipublic.blob.core.windows.net/encodings/o200k_base.tiktoken',
+            'pat' => '/[^\r\n\p{L}\p{N}]?[\p{Lu}\p{Lt}\p{Lm}\p{Lo}\p{M}]*[\p{Ll}\p{Lm}\p{Lo}\p{M}]+(?i:\'s|\'t|\'re|\'ve|\'m|\'ll|\'d)?|[^\r\n\p{L}\p{N}]?[\p{Lu}\p{Lt}\p{Lm}\p{Lo}\p{M}]+[\p{Ll}\p{Lm}\p{Lo}\p{M}]*(?i:\'s|\'t|\'re|\'ve|\'m|\'ll|\'d)?|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n\/]*|\s*[\r\n]+|\s+(?!\S)|\s+/u',
+        ],
     ];
     private const MODEL_PREFIX_TO_ENCODING = [
+        'gpt-4o-' => 'o200k_base',
         'gpt-4-' => 'cl100k_base',
         'gpt-3.5-turbo-' => 'cl100k_base',
     ];
     private const MODEL_TO_ENCODING = [
+        'gpt-4o' => 'o200k_base',
         'gpt-4' => 'cl100k_base',
         'gpt-3.5-turbo' => 'cl100k_base',
+        'gpt-3.5' => 'cl100k_base',
         'text-davinci-003' => 'p50k_base',
         'text-davinci-002' => 'p50k_base',
         'text-davinci-001' => 'r50k_base',
