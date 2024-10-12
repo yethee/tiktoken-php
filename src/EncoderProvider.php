b399a17a1a3430f7b622e6937ffb29a23a9441ff
Arsen Ibragimov <barsnur@gmail.com>
2024-10-12 20:39:08 +0200

    add support for o1 models

--- src/EncoderProvider.php
+++ src/EncoderProvider.php
@@ -54,6 +54,7 @@ final class EncoderProvider implements ResetInterface
         'gpt-3.5-turbo-' => 'cl100k_base',
     ];
     private const MODEL_TO_ENCODING = [
+        'chatgpt-4o-': 'o200k_base',
         'gpt-4o' => 'o200k_base',
         'gpt-4' => 'cl100k_base',
         'gpt-3.5-turbo' => 'cl100k_base',
